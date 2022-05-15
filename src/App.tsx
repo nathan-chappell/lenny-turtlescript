3c9d545a4a351fa77c73d01cc9f71d530bf344e0
Nathan Chappell <nathan.s.chappell@gmail.com>
2022-05-15 22:00:12 +0200

    initial drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import { SimpleRenderer } from "./render/simpleRenderer";
+import { Turtle } from "./turtles/turtle";
+import { createWhile } from "typescript";
+
+const turtle = new Turtle();
+
+turtle.notifer.subscribe('log', o => console.log(o));
+
+const delay = (t: number) => new Promise(res => window.setTimeout(res, t));
+const crawl =async () => {
+  turtle.moveForward(50);
+  await delay(500);
+  turtle.moveForward(50);
+  await delay(500);
+  turtle.turn(-45);
+  turtle.moveForward(50);
+  await delay(500);
+  turtle.moveForward(50);
+  await delay(500);
+}
+
+crawl();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+      const ctx = canvasRef.current?.getContext('2d');
+      if (ctx) {
+        new SimpleRenderer(ctx, turtle);
+      }
+    }, [canvasRef]);
+
+    return (
+        <div className="App">
+            <canvas width={400} height={400} ref={canvasRef} />
+        </div>
+    );
 }
 
 export default App;
--- src/render/simpleRenderer.ts
+++ src/render/simpleRenderer.ts
@@ -5,7 +5,7 @@ export class SimpleRenderer {
     private _ctx: CanvasRenderingContext2D;
     private _turtle: Turtle;
     private _id: string = 'simple-renderer'
-    private _scale: number = 200;
+    private _scale: number = 50;
 
     private positionSequence: [number, number][] = [];
 
@@ -16,17 +16,25 @@ export class SimpleRenderer {
     }
 
     connect() {
+        this._ctx.strokeStyle = '#000';
+        this._ctx.lineWidth = 10;
+
+        this._ctx.beginPath();
+        this._ctx.moveTo(200,200);
         this._turtle.notifer.subscribe(this._id, (turtle: Turtle, args: EventArgs) => {
-            this.positionSequence.push([turtle.x, turtle.y]);
-            this.render();
+            // this.positionSequence.push([turtle.x, turtle.y]);
+            // this.render();
+            this._ctx.lineTo(200 + turtle.x,200 + turtle.y);
+            this._ctx.stroke();
         });
     }
 
     render() {
+        
         this._ctx.moveTo(0,0);
+        this._ctx.beginPath();
         for (let [x, y] of this.positionSequence) {
-            let [sx, sy] = this.scale(x,y);
-            this._ctx.moveTo(sx,sy);
+            this._ctx.lineTo(x,y);
         }
         this._ctx.stroke();
     }
